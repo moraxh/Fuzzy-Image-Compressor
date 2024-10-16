51626a250e74b00dba35c9649e41d5fdf4f57c0a
Jorge Mora <jorgemorah671@gmail.com>
2024-10-15 19:39:09 -0600

    feat: :sparkles: Basic image read

--- src/main.c
+++ src/main.c
@@ -1,5 +1,21 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello World");
+    FILE *fptr;
+    fptr = fopen("images/image2.png", "rb");
+
+    if (fptr == NULL) {
+        printf("Error opening file");
+        return 1;
+    }
+
+    fseek(fptr, 0, SEEK_SET);
+
+    char buffer[100];
+
+    size_t bytesRead = fread(buffer, sizeof(char), 100, fptr);
+
+    printf("%zu", bytesRead);
+
+    fclose(fptr);
 }
